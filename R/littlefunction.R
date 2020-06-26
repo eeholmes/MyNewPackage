b5b87cc62251f380b37535a5fe0891f0ef6c5ad1
Eli Holmes <eli.holmes@noaa.gov>
2020-06-26 16:46:34 -0700

    add documentation

--- R/littlefunction.R
+++ R/littlefunction.R
@@ -1,3 +1,17 @@
+#' Make a ARIMA forecast
+#'
+#' This function takes a vector or time series of data,
+#' fits a ARIMA function (selected with AIC), and
+#' plots the data. This is different than \link{hello}.
+#'
+#' @param data This is a time-series of data. Must be a vector
+#' or a ts object.
+#' @param nyears Number of years in the future to forecast.
+#' Must > 0.
+#'
+#' @examples
+#' littleforecast(WWW2)
+#' @export
 littleforecast <- function(data, nyears=10){
   fit <- forecast::auto.arima(data)
   fc <- forecast::forecast(fit, h = nyears)
